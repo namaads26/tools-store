d4bf71632f68ba4a3dcb25edcee0a159b740c2f7
namaads26 <nama.ads26@gmail.com>
2026-04-22 19:06:17 -0300

    conectamos firebase

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,9 +1,14 @@
 import { initializeApp } from "firebase/app";
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "xxx",
-  projectId: "xxx",
+  apiKey: "AIzaSyAHlvzyPNGLcVA2X9NmsnpVogoO8zGKZBg",
+  authDomain: "tools-store-5ccf6.firebaseapp.com",
+  projectId: "tools-store-5ccf6",
+  storageBucket: "tools-store-5ccf6.firebasestorage.app",
+  messagingSenderId: "969624338520",
+  appId: "1:969624338520:web:b839da857a7d117c528a2d",
+  measurementId: "G-2QT58BWZ26"
 };
 
 export const app = initializeApp(firebaseConfig);
